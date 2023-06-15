bff84fa0fab6f4bbd99c3654a2b71cb7a3a8abe3
LakshyaChauhan <clakshya923@gmail.com>
2023-06-15 10:24:29 +0530

    normal changes

--- lib/hompeage.dart
+++ lib/hompeage.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:project1/browse.dart';
+import 'package:project1/widgets/browse.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-
-import 'package:project1/capture.dart';
+import 'package:project1/widgets/capture.dart';
 import 'package:project1/upload_image.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,13 +14,6 @@ class HomePage extends StatefulWidget {
   }
 }
 
-
-class _HomePageState extends State<HomePage> {
-  File? selectedImage;
-
-
-
-
 class _HomePageState extends State<HomePage> {
   File? selectedImage;
 
@@ -31,95 +23,35 @@ class _HomePageState extends State<HomePage> {
     if (pickedFile == null) return;
 
     final tempImage = File(pickedFile.path);
+
     setState(() {
       selectedImage = tempImage;
       print('image got selected');
       Navigator.push(
           context,
           MaterialPageRoute(
-              builder: (context) => Upload_Image(image: selectedImage)));
+              builder: (context) => UploadImage(image: selectedImage)));
     });
-
-
-    selectedImage = tempImage;
-    if (selectedImage != null) {
-      print('jello world');
-    }
-
   }
 
   @override
   Widget build(BuildContext context) {
-    
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Project'),
-      ),
-      body: SizedBox(
-        height: double.infinity,
-        width: double.infinity,
-        child: Center(
-
+        appBar: AppBar(
+          title: const Text('Project'),
+        ),
+        body: SizedBox(
+          width: double.infinity,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Capture(camera: capture),
-              SizedBox(
+              const SizedBox(
                 height: 50,
               ),
-              Browse(gallery: capture)
+              Browse(gallery: capture),
             ],
-
-          child: Container(
-            height: 70,
-            width: screenWidth - 80,
-            padding: const EdgeInsets.all(7),
-            child: ElevatedButton.icon(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    content: Container(
-                      width: 100,
-                      height: 230,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30)),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Capture(camera: capture),
-                          const SizedBox(
-                            width: 30,
-                          ),
-                          Browse(
-                            gallery: capture,
-                          ),
-                          
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              },
-              icon: const Icon(
-                Icons.image,
-                color: Colors.white,
-                size: 32,
-              ),
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 0, 96, 160)),
-              label: Text(
-                'Select Image',
-                style: GoogleFonts.breeSerif(
-                    color: const Color.fromARGB(255, 255, 255, 255),
-                    fontSize: 20),
-              ),
-            ),
-
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
--- lib/upload_image.dart
+++ lib/upload_image.dart
@@ -5,112 +5,63 @@ import 'package:flutter/material.dart';
 
 import 'fetch_Image_size.dart';
 
-class Upload_Image extends StatelessWidget {
+class UploadImage extends StatelessWidget {
   final File? image;
-  const Upload_Image({Key? key, required this.image}) : super(key: key);
+  const UploadImage({Key? key, required this.image}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-                width: screenWidth - 60,
-                child: Column(
-                  children: [
-                    Image.file(
-                      File(image!.path).absolute,
-                      fit: BoxFit.cover,
-                    ),
-                    const SizedBox(
-                      height: 25,
-                    ),
-                    const Text(
-                      'Selected Image',
+      body: Center(
+        child: SizedBox(
+          width: screenWidth - 60,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.file(
+                File(image!.path).absolute,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              const Text(
+                'Selected Image',
+                style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: screenWidth - 150,
+                child: ElevatedButton(
+                    onPressed: () {
+                      String imagePath = image!.path;
+                      fetchImageSize(imagePath)
+                          .then((size) =>
+                              print('The size of the image is: $size'))
+                          .catchError((error) => print('Error: $error'));
+                    },
+                    style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        backgroundColor: CupertinoColors.activeBlue),
+                    child: const Text(
+                      'Upload Image',
                       style: TextStyle(
-                        fontSize: 23,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    )
-                  ],
-                )),
-            const SizedBox(
-              height: 50,
-            ),
-            SizedBox(
-              width: screenWidth - 150,
-              child: ElevatedButton(
-                  onPressed: () {
-                    String imagePath = image!.path;
-                    fetchImageSize(imagePath)
-                        .then(
-                            (size) => print('The size of the image is: $size'))
-                        .catchError((error) => print('Error: $error'));
-                  },
-                  style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      backgroundColor: CupertinoColors.activeBlue),
-                  child: const Text(
-                    'Upload Image',
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w900),
-                  )),
-            )
-          ],
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w900),
+                    )),
+              )
+            ],
+          ),
         ),
       ),
     );
-    // return SizedBox(
-    //   width: double.infinity,
-    //   child: Column(
-    //     mainAxisAlignment: MainAxisAlignment.center,
-    //     children: [
-    //       Container(
-    //         margin: const EdgeInsets.symmetric(horizontal: double.infinity -20),
-    //         child: Image.file(
-    //           File(image!.path).absolute,
-    //           width: 225,
-    //           height: 260,
-    //         ),
-    //       ),
-    //       const Text(
-    //         'Selected Image',
-    //         style: TextStyle(
-    //             fontSize: 20, fontWeight: FontWeight.w800, color: Colors.white),
-    //       ),
-    //       const SizedBox(
-    //         height: 70,
-    //       ),
-    //       SizedBox(
-    //         width: 170,
-    //         height: 60,
-    //         child: ElevatedButton(
-    //             onPressed: () {
-    //               String imagePath = image!.path;
-    //               fetchImageSize(imagePath)
-    //                   .then((size) => print('The size of the image is: $size'))
-    //                   .catchError((error) => print('Error: $error'));
-    //             },
-    //             style: ElevatedButton.styleFrom(
-    //                 backgroundColor: CupertinoColors.activeBlue),
-    //             child: const Text(
-    //               'Upload Image',
-    //               style: TextStyle(
-    //                   fontSize: 18,
-    //                   color: Colors.white,
-    //                   fontWeight: FontWeight.w900),
-    //             )),
-    //       )
-    //     ],
-    //   ),
-
-    // );
   }
 }
