58bb81afaba5b43d90bf83ee5940f8b6c58ed542
LakshyaChauhan <clakshya923@gmail.com>
2023-06-19 15:46:10 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'package:project1/screens/Splash_Screen.dart';
+import 'package:project1/screens/splash_screen.dart';
 
 
 
--- lib/screens/Splash_Screen.dart
+++ lib/screens/Splash_Screen.dart
@@ -1,71 +1,62 @@
-import 'dart:async';
 
 
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:project1/screens/hompeage.dart';
 import 'package:project1/screens/registration_onboarding_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
- int ?status;
-Future<void> Get_status() async{
+
+int? status;
+Future<void> getStatus() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences preferences= await SharedPreferences.getInstance();
-  status =await preferences.getInt('onboarding_status');
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  status = preferences.getInt('onboarding_status');
 }
 
-
-
-
-
-
-
 class Splashpage extends StatefulWidget {
-  const Splashpage({Key? key, }) : super(key: key);
+  const Splashpage({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  _SplashpageState createState() => _SplashpageState();
+  State<StatefulWidget> createState() {
+    return _SplashpageState();
+  }
 }
 
 class _SplashpageState extends State<Splashpage> {
-
-
+  @override
+  void initState() {
+    getStatus();
+    Timer(const Duration(seconds: 4), () {
+      if (status == 0 || status == null) {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const OnboardingScreen()));
+      } else {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => const HomePage()));
+      }
+    });
+    super.initState();
+  }
 
   @override
-
-    void initState() {
-    Get_status();
-      Timer(const Duration(seconds: 4), () {
-        if (status == 0 || status == null) {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const OnboardingScreen()));
-        }
-        else {
-          Navigator.pushReplacement(
-              context, MaterialPageRoute(builder: (context) => const HomePage()));
-        }
-      });
-      super.initState();
-    }
-    @override
-    Widget build(BuildContext context) {
-      final screenWidth = MediaQuery
-          .of(context)
-          .size
-          .width;
-      return Container(
-        width: screenWidth,
-        alignment: Alignment.center,
-
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: ClipRRect(
-              borderRadius: BorderRadius.circular(150),
-              child: Image.asset(
-                'assets/dic_logo.png', width: screenWidth - 100,)),
-
-        ),
-      );
-    }
-
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Container(
+      width: screenWidth,
+      alignment: Alignment.center,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(150),
+            child: Image.asset(
+              'assets/dic_logo.png',
+              width: screenWidth - 100,
+            )),
+      ),
+    );
+  }
 }
 //
 
