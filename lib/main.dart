1b60943617782ef32f84e29650687fe002b7adad
LakshyaChauhan <clakshya923@gmail.com>
2023-06-19 13:48:40 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:project1/widgets/Splash_Screen.dart';
+import 'package:project1/screens/Splash_Screen.dart';
 
 void main() {
   runApp( GetMaterialApp(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:project1/hompeage.dart';
+import 'package:project1/screens/hompeage.dart';
 
 // import 'package:project1/main.dart';
 
